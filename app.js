a4c0d3fc9e41a6e082ffe948c6c5d2f088056a9a
aiswaa67 <aiswatunkholifah@gmail.com>
2024-10-30 10:51:15 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-function runner() {
+// function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+  let tinggiBadan = ["150", "155", "148", "165", "170", "168", "166"]
+  let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+  for (let i = 0; i < 7 ; i++) {
+
+    console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]} cm`)
+  }
 
 
+  // JANGAN UBAH SELAINNYA
+// } 
 
-
-
-
-  /// JANGAN UBAH SELAINNYA
-}
-
-module.exports = runner
+// module.exports = runner
